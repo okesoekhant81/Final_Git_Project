99577c1c928d235d352874419221e3b46b1ee039
okesoekhant81 <111676956+okesoekhant81@users.noreply.github.com>
2022-09-14 14:53:40 +0630

    Update Simple_interest.sh
    Fix the script

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,29 +1,28 @@
-while [ 1 ]
-do
-echo "MENU"
-echo "1. Simple Interest"
-echo "2. Compound Interest"
-echo "3. Exit"
-echo "Enter Your Choice"
-read choice
-case $choice in
-1) echo "Enter the values: Principal Amount,Time (In Years) and Rate of Interest"
-read p
-read t
-read r
-si=`echo "scale=2;( $p * $t * $r ) / 100" | bc`
-echo "Simple Interest = $si" ;;
-2) echo "Enter the values: Principal Amount,Time (In Years),Rate of Interest and
-number of times interest applied per year"
-read p
-read t
-read r
-read n
-power=`expr $n \* $t`
-echo $power
-ci=`echo "scale=2; $p * ( 1 + $r / $n ) ^ $power" | bc`
-echo "Compound Interest = $ci" ;;
-3) exit;;
-*) echo "Invalid Choice";;
-esac
-done
+#!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+
+# Do not use this in production. Sample purpose only.
+
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# A name
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r
+
+echo"Enter the principal:"
+read p    
+echo"Enter rate of interest per year:"
+read r    
+echo"Enter time period in years:"
+read t     
+
+s=`expr $p \* $t \* $r / 100`    
+echo"The simple interest is: "
+echo$s
